403a51e1bf829c7994ac44d0109391467214c0f2
Jonathan Johnson <me@jondavidjohn.com>
2022-09-07 11:00:59 -0500

    remove unecessary stuff

--- run_triggers.tf
+++ run_triggers.tf
@@ -1,71 +1,71 @@
 locals {
-  triggereds = {for t in tfe_workspace.run_triggered_workspaces: index(tfe_workspace.run_triggered_workspaces, t) => t.id}
-  triggerings = {for t in tfe_workspace.run_triggering_workspaces: index(tfe_workspace.run_triggering_workspaces, t) => t.id}
+  #  triggereds = {for t in tfe_workspace.run_triggered_workspaces: index(tfe_workspace.run_triggered_workspaces, t) => t.id}
+  #  triggerings = {for t in tfe_workspace.run_triggering_workspaces: index(tfe_workspace.run_triggering_workspaces, t) => t.id}
 }
 
-# --- Sets up the "run-triggered" workspace where we only have other workspaces triggering runs
-
-resource "tfe_workspace" "run_triggered_workspace" {
-  organization      = var.organization_name
-  name              = "run-triggered"
-}
-
-resource "tfe_workspace" "run_triggering_workspaces" {
-  count = 16
-  organization      = var.organization_name
-  name              = "run-triggerings-${count.index}"
-}
-
-resource "tfe_run_trigger" "run_trigger_triggered" {
-  for_each      = local.triggerings
-  sourceable_id = each.value
-  workspace_id  = tfe_workspace.run_triggered_workspace.id
-}
-
-# --- Sets up the "run-triggering" workspace where we only are triggering other workspaces
-
-resource "tfe_workspace" "run_triggering_workspace" {
-  organization      = var.organization_name
-  name              = "run-triggering"
-}
-
-resource "tfe_workspace" "run_triggered_workspaces" {
-  count = 16
-  organization      = var.organization_name
-  name              = "run-triggereds-${count.index}"
-}
-
-resource "tfe_run_trigger" "run_trigger_triggering" {
-  for_each      = local.triggereds
-  sourceable_id = tfe_workspace.run_triggering_workspace.id
-  workspace_id  = each.value
-}
-
-# --- Sets up the "run-triggers" workspace where we have both inbound and outbound triggers.
-
-resource "tfe_workspace" "run_triggers_workspace" {
-  organization      = var.organization_name
-  name              = "run-triggers"
-}
-
-resource "tfe_workspace" "long_name_workspace" {
-  organization      = var.organization_name
-  name              = "this-is-an-unreasonbly-long-workspace-name-that-has-enough-characters-to-truncate"
-}
-
-resource "tfe_run_trigger" "run_trigger_long_name" {
-  sourceable_id = tfe_workspace.run_triggers_workspace.id
-  workspace_id  = tfe_workspace.long_name_workspace.id
-}
-
-resource "tfe_run_trigger" "run_trigger_both_a" {
-  for_each      = local.triggereds
-  sourceable_id = each.value
-  workspace_id  = tfe_workspace.run_triggers_workspace.id
-}
-
-resource "tfe_run_trigger" "run_trigger_both_b" {
-  for_each      = local.triggerings
-  sourceable_id = tfe_workspace.run_triggers_workspace.id
-  workspace_id  = each.value
-}
+## --- Sets up the "run-triggered" workspace where we only have other workspaces triggering runs
+#
+#resource "tfe_workspace" "run_triggered_workspace" {
+#  organization      = var.organization_name
+#  name              = "run-triggered"
+#}
+#
+#resource "tfe_workspace" "run_triggering_workspaces" {
+#  count = 16
+#  organization      = var.organization_name
+#  name              = "run-triggerings-${count.index}"
+#}
+#
+#resource "tfe_run_trigger" "run_trigger_triggered" {
+#  for_each      = local.triggerings
+#  sourceable_id = each.value
+#  workspace_id  = tfe_workspace.run_triggered_workspace.id
+#}
+#
+## --- Sets up the "run-triggering" workspace where we only are triggering other workspaces
+#
+#resource "tfe_workspace" "run_triggering_workspace" {
+#  organization      = var.organization_name
+#  name              = "run-triggering"
+#}
+#
+#resource "tfe_workspace" "run_triggered_workspaces" {
+#  count = 16
+#  organization      = var.organization_name
+#  name              = "run-triggereds-${count.index}"
+#}
+#
+#resource "tfe_run_trigger" "run_trigger_triggering" {
+#  for_each      = local.triggereds
+#  sourceable_id = tfe_workspace.run_triggering_workspace.id
+#  workspace_id  = each.value
+#}
+#
+## --- Sets up the "run-triggers" workspace where we have both inbound and outbound triggers.
+#
+#resource "tfe_workspace" "run_triggers_workspace" {
+#  organization      = var.organization_name
+#  name              = "run-triggers"
+#}
+#
+#resource "tfe_workspace" "long_name_workspace" {
+#  organization      = var.organization_name
+#  name              = "this-is-an-unreasonbly-long-workspace-name-that-has-enough-characters-to-truncate"
+#}
+#
+#resource "tfe_run_trigger" "run_trigger_long_name" {
+#  sourceable_id = tfe_workspace.run_triggers_workspace.id
+#  workspace_id  = tfe_workspace.long_name_workspace.id
+#}
+#
+#resource "tfe_run_trigger" "run_trigger_both_a" {
+#  for_each      = local.triggereds
+#  sourceable_id = each.value
+#  workspace_id  = tfe_workspace.run_triggers_workspace.id
+#}
+#
+#resource "tfe_run_trigger" "run_trigger_both_b" {
+#  for_each      = local.triggerings
+#  sourceable_id = tfe_workspace.run_triggers_workspace.id
+#  workspace_id  = each.value
+#}
--- teams.tf
+++ teams.tf
@@ -1,40 +1,40 @@
-resource "tfe_workspace" "teams_workspace" {
-  organization      = var.organization_name
-  name              = "teams"
-}
-
-resource "tfe_team" "visible_teams" {
-  count        = 50
-  name         = "team-${count.index}"
-  organization = var.organization_name
-  visibility   = "organization"
-}
-
-resource "tfe_team" "secret_team" {
-  name         = "secret-team"
-  organization = var.organization_name
-}
-
-resource "tfe_team_access" "team_workspace_access" {
-  for_each     = toset([for team in tfe_team.visible_teams : team.id])
-  team_id      = each.value
-  access       = "read"
-  workspace_id = tfe_workspace.teams_workspace.id
-}
-
-resource "tfe_team_access" "secret_team_workspace_access" {
-  access       = "read"
-  team_id      = tfe_team.secret_team.id
-  workspace_id = tfe_workspace.teams_workspace.id
-}
-
-resource "tfe_team_organization_member" "visible_team_user_membership" {
-  for_each                   = toset([for team in tfe_team.visible_teams : team.id])
-  team_id                    = each.value
-  organization_membership_id = data.tfe_organization_membership.user.id
-}
-
-resource "tfe_team_organization_member" "secret_team_user_membership" {
-  team_id                    = tfe_team.secret_team.id
-  organization_membership_id = data.tfe_organization_membership.user.id
-}
+#resource "tfe_workspace" "teams_workspace" {
+#  organization      = var.organization_name
+#  name              = "teams"
+#}
+#
+#resource "tfe_team" "visible_teams" {
+#  count        = 50
+#  name         = "team-${count.index}"
+#  organization = var.organization_name
+#  visibility   = "organization"
+#}
+#
+#resource "tfe_team" "secret_team" {
+#  name         = "secret-team"
+#  organization = var.organization_name
+#}
+#
+#resource "tfe_team_access" "team_workspace_access" {
+#  for_each     = toset([for team in tfe_team.visible_teams : team.id])
+#  team_id      = each.value
+#  access       = "read"
+#  workspace_id = tfe_workspace.teams_workspace.id
+#}
+#
+#resource "tfe_team_access" "secret_team_workspace_access" {
+#  access       = "read"
+#  team_id      = tfe_team.secret_team.id
+#  workspace_id = tfe_workspace.teams_workspace.id
+#}
+#
+#resource "tfe_team_organization_member" "visible_team_user_membership" {
+#  for_each                   = toset([for team in tfe_team.visible_teams : team.id])
+#  team_id                    = each.value
+#  organization_membership_id = data.tfe_organization_membership.user.id
+#}
+#
+#resource "tfe_team_organization_member" "secret_team_user_membership" {
+#  team_id                    = tfe_team.secret_team.id
+#  organization_membership_id = data.tfe_organization_membership.user.id
+#}
